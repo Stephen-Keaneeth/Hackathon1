44424bc988ac37f6e4e95c0cd69e21e0d5687df9
Stephen-Keaneeth <stephenkeanny27@gmail.com>
2025-09-19 23:36:59 +0530

    Delete frontend directory

--- frontend/sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE = 'v1';
-const urls = ['/', '/index.html', '/styles.css', '/script.js'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(urls)));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
